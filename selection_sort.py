e5685e84da68787fe0d2d42a2348a21296a7b302
steven <slstevenlam@gmail.com>
2015-09-09 16:33:53 -0700

    update selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -3,24 +3,33 @@
 #start with linear selection
 #iterate through array and find minimum value
 #swap variable position with minimum value
+import time
+start_time = time.time()
+
 
 my_list = [6, 5, 2, 8, 3, 1, 4, 7, 9]
 print "length of list %s" % len(my_list)
 def selection_sort(my_list):
 	leng = len(my_list)
 	for i in range(leng):
+		max = my_list[i]
 		min = my_list[i]
 		val_index = i
 		#print "i %s" % i
 		for j in range(i,leng):
-			#print "j %s" % j
+			#find the min value in list
 			if my_list[j] <= min:
 				min = my_list[j]
 				min_index = j
+			#find max value in list
+			if my_list[j] <= max:
+				max = my_list[j]
+				
 			if j == leng-1:
 				#value of index is = i
 				#min_index = index of the found min in my list
 				after_swap = swap(my_list, val_index, min_index)
+	print "max %s" % max	
 	print after_swap
 
 
@@ -35,3 +44,5 @@ def swap(my_list, val_index, min_index):
 
 
 selection_sort(my_list)
+
+print("--- %s seconds ---" % (time.time() - start_time))
